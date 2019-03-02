93496fdcd5f531dd23847990ad5abb7bfb25b33e
Seongjun <metalg0su@gmail.com>
2019-03-02 13:39:32 +0900

    [etc/install-ubuntu] some eye-candies. on-going commit

--- settings/installs/ubuntu-packages.sh
+++ settings/installs/ubuntu-packages.sh
@@ -1,10 +1,32 @@
-##------ apt install list
-# what I want
+##----- for the unix-Porn!! 
+#--- terminal
 tmux
 tmuxinator
 vim
-
-# uni-xPorn --; 
 neofetch
 
+##----- graphical themes
+#--- basics
+gnome-tweak-tool
+gnome-shell-extensions
+chrome-gnome-shell
+gnome-shell-extension-dashtodock 
+
+#--- themes 
+# repositories
+ppa:daniruiz/flat-remix # flat-remix
+https://github.com/vinceliuice/vimix-gtk-themes
+
+# names
+flat-remix-gnome
+arc-theme 
+vimix
+
+#--- icon
+# repositories
+ppa:numix/ppa # numix
+ppa:noobslab/icons # dalisha
+
+# names
+numix-icon-theme-circle
 
