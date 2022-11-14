07b0af5d733b9c851356f700bde7e7f0d3e65d41
Seongjun Ji <metalg0su@gmail.com>
2022-11-14 16:35:06 +0900

    auto commit

--- settings/bashrc.d/env/terminal_ps.sh
+++ settings/bashrc.d/env/terminal_ps.sh
@@ -4,3 +4,6 @@ parse_git_branch() {
 }
 export PS1="\[\e[33m\]\u\[\e[m\]@\[\e[32;40m\]\h\[\e[m\]:\[\e[31m\]\w\[\e[m\]\[\e[36m\]\$(parse_git_branch)\\$\[\e[m\] "
 
+export CLICOLOR=1
+export LSCOLORS=ExGxFxdxCxDxDxhbadExEx
+# 출처: https://gnuteam.tistory.com/93 [소프트웨어 견습생의 필살 로그]
