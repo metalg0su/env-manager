e0e4d5ce04f75d0c91c25dc7ed256dde10234632
지성준 <seongjun.ji@greencar.co.kr>
2023-01-19 16:50:58 +0900

    f

--- settings/bashrc.d/env/env.sh
+++ settings/bashrc.d/env/env.sh
@@ -2,10 +2,52 @@
 export EDITOR=vim
 export TERM=xterm-256color
 set -o vi  # vi mode
-parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+
+get_unstaged_symbol() {
+	count=($(git diff --name-only | wc -l))
+	if [[ "${count}" -ne 0 ]]; then
+		echo "*${count}"
+	fi
 }
-export PS1="\[\e[33m\]\u\[\e[m\]@\[\e[32;40m\]\h\[\e[m\]:\[\e[31m\]\w\[\e[m\]\[\e[36m\]\$(parse_git_branch)\\$\[\e[m\] "
+
+get_staged_symbol() {
+	count=($(git diff --name-only --cached | wc -l))
+	if [[ "${count}" -ne 0 ]]; then
+		echo "+${count}"
+	fi
+}
+
+get_current_branch_symbol() {
+	git symbolic-ref -q HEAD &> /dev/null
+	if [[ $? -ne "0" ]]; then
+		echo "✖ $(git rev-parse --short HEAD)"
+	else
+		echo "$(git branch --show-current)"
+	fi
+}
+
+parse_git_branch() {
+	git branch &> /dev/null
+	if [[ $? -ne "0" ]]; then
+		return 0;
+	fi
+
+	result="($(get_current_branch_symbol))"
+
+	unstaged=$(get_unstaged_symbol)
+	if [[ -n "${unstaged}" ]]; then
+		result="${result} ${unstaged}"
+	fi
+
+	staged=$(get_staged_symbol)
+	if [[ -n "${staged}" ]]; then
+		result="${result} ${staged}"
+	fi
+
+	echo "${result}"
+}
+
+export PS1="\[\e[33m\]\u\[\e[m\]@\[\e[32;40m\]\h\[\e[m\]:\[\e[31m\]\w\[\e[m\]\[\e[36m\] \$(parse_git_branch)\\$\[\e[m\] "
 export CLICOLOR=1
 export LSCOLORS=ExGxFxdxCxDxDxhbadExEx
 
