bfba49da62a3f3970c79b780d76a93503ad28589
metalg0su <metalg0su@gmail.com>
2025-09-24 17:43:59 +0900

    remove all files

--- settings/bashrc.d/aliases/at_ubuntu.sh
+++
@@ -1,6 +0,0 @@
-distro=$(uname)
-
-if [ $distro == "Linux" ]; then
-	alias pbcopy="xclip -sel clipboard"
-	alias open="xdg-open"
-fi
--- settings/bashrc.d/env/pyenv.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-export PATH="$PATH:$HOME/.pyenv/bin"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-export PYENV_VERSION=system
